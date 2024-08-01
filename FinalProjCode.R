fd3cf74b63b0c577629b3049da5b45e09ccc57f3
Jasper Dong <116551997+jmudong@users.noreply.github.com>
2024-08-01 03:35:27 -0700

    Update FinalProjCode.R

--- FinalProjCode.R
+++ FinalProjCode.R
@@ -1,6 +1,30 @@
 library(MASS)
 
-x = 5
-y = 2
-z = 7
-a = 1
+# Creating hyperlink matrix
+G = t(fractions(matrix(c(0, 0, 0, 1/2, 0, 0, 0, 1/2, 0, 0, 0, 0,
+                       1/3, 0, 0, 1/3, 1/3, 0, 0, 0, 0, 0, 0, 0,
+                       0, 1/2, 0, 0, 1/2, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 1/2, 0, 1/2, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 1/4, 1/4, 1/4, 0, 1/4, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 1/2, 0, 1/2, 0,
+                       0, 0, 0, 0, 0, 0, 1/2, 0, 0, 0, 1/2, 0,
+                       0, 0, 0, 0, 0, 1/2, 1/2, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 1/3, 0, 0, 0, 1/3, 0, 1/3,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+                       nrow = 12, ncol = 12)))
+
+# Eigenvalue Decomposition
+ev1 = eigen(t(G))
+evalue1 = ev1$values[1] # Choosing largest eigenvalue
+evector1 = ev1$vectors[,1] # Eigenvector corresponding to largest eigenvalue
+
+# Adding artificial links to dangling node
+Gnew = G
+Gnew[5,] = rep(1/12, 12)
+ev2 = eigen(t(Gnew)) # Eigenvalue decomposition
+evalue2 = ev2$values[1]
+evector2 = ev2$vectors[,1]
+
+
